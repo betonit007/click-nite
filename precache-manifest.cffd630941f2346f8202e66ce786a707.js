8df779047d70f9515a239e269dc2ad8064bc284a
betonit007 <timnagorski@hotmail.com>
2019-01-27 15:05:30 -0500

    Updates

---
+++ precache-manifest.cffd630941f2346f8202e66ce786a707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e841e0a7aa37a4d30d0e",
+    "url": "/click-nite/static/js/runtime~main.e841e0a7.js"
+  },
+  {
+    "revision": "5ae42781bb9d756fcd49",
+    "url": "/click-nite/static/js/main.5ae42781.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/click-nite/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "5ae42781bb9d756fcd49",
+    "url": "/click-nite/static/css/main.378acdc1.chunk.css"
+  },
+  {
+    "revision": "d14fc2e0bc5cebc848809d489f86861f",
+    "url": "/click-nite/index.html"
+  }
+];
